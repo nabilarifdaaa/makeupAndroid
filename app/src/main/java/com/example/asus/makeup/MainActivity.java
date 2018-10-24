03fa296f0aa5049c5738939357f28bd5a1e68fed
LAPTOP-4CTHJB70\Asus <nabilarifdaaa@gmail.com>
2018-10-24 12:16:38 +0700

    new

--- app/src/main/java/com/example/asus/makeup/MainActivity.java
+++ app/src/main/java/com/example/asus/makeup/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.asus.makeup;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(MainActivity.this, Create.class);
+                startActivity(myintent);
+            }
+        });
     }
 }
